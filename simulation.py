11e95b5b3fe180e4791a0fb159df729009fe0e35
Augnik Banerjee <74064653+Augnik03@users.noreply.github.com>
2023-10-26 22:11:43 +0530

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -4,16 +4,16 @@ import threading
 import pygame
 import sys
 
-# Default values of signal timers
+
 defaultGreen = {0:10, 1:10, 2:10, 3:10}
 defaultRed = 150
 defaultYellow = 5
 
 signals = []
 noOfSignals = 4
-currentGreen = 0   # Indicates which signal is green currently
-nextGreen = (currentGreen+1)%noOfSignals    # Indicates which signal will turn green next
-currentYellow = 0   # Indicates whether yellow signal is on or off 
+currentGreen = 0   
+nextGreen = (currentGreen+1)%noOfSignals    
+currentYellow = 0   
 
 speeds = {'car':2.25, 'bus':1.8, 'truck':1.8, 'bike':2.5}  # average speeds of vehicles
 
